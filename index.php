35f5fec958ce3898767c6b743f6b234a07a28815
Resh1602 <reshmasatheesh16@gmail.com>
2017-07-23 16:03:53 +0530

    Initial file

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+	class Home{
+		
+		function __construct(){
+			
+			if(!isset($_SESSION['items'])) $_SESSION['items'] =  array();
+			
+		}
+		
+		function getItem(){
+			
+			return isset($_GET['t1']) ? $_GET['t1'] :  false;
+			
+		}
+		
+		function addToList($title = ''){
+			
+			array_push( $_SESSION['items'], $title);
+			
+		}
+		
+		function display(){
+			
+			return isset($_SESSION['items']) ? $_SESSION['items'] :  array();
+			
+		}
+		
+		
+	}
+	
+	$myApp = new Home();
+	$item = $myApp->getItem();
+	if($item!=false) $myApp->addToList($item);
+	
+?>
+
+<html>	
+	<body>
+		<form name="f1" method="get" action="">
+			Item <input type="text" name="t1" id="txt1">
+			<input type="submit" value="Add"><br><br>
+			<ul>
+			<?php foreach($myApp->display() as $item){ ?>
+				<?= '<li>' . $item . '</li>'; ?>
+			<?php } ?>
+			</ul>
+		</form>
+	</body>
+</html>
